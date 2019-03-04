7ea9f02eaaa828ebcfc5d3713fb952e53b53aa4b
griegomas <38501062+griegomas@users.noreply.github.com>
2019-03-03 21:16:14 -0500

    Add files via upload

---
+++ client/build/precache-manifest.b7fcf30e0ea1a69dfebb25b5db353b94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a68e411f99a31a88ef2",
+    "url": "/static/js/main.3a68e411.chunk.js"
+  },
+  {
+    "revision": "70d5445ab58e6b834b25",
+    "url": "/static/js/1.70d5445a.chunk.js"
+  },
+  {
+    "revision": "3a68e411f99a31a88ef2",
+    "url": "/static/css/main.d24c8d1d.chunk.css"
+  },
+  {
+    "revision": "18ad83f3556e4017371a4cb9588b77f6",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Built in REACT
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
